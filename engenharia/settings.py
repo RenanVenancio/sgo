31b36240305d05883222f444a390d13e729ba1fa
Renan Venâncio <renan_1419@hotmail.com>
2019-05-21 21:12:26 -0300

    Configurando sistema para testes com Mysql

--- engenharia/settings.py
+++ engenharia/settings.py
@@ -114,12 +114,30 @@ USE_TZ = True
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'db.sqlite3',
     }
 }
+'''
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'engenhar_sistemadegestao',
+        'USER': 'root',
+        'PASSWORD': 'rv101220',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'default-character-set': 'utf8',
+        #'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    }
+}
+
+
 
 ADMINS = (
     ('Victor Fernandes', 'victorfernandes.matias@gmail.com'),
