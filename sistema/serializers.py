ddfd3014aadc14041e09e260025f3f1fc680bb0e
Renan Venâncio <renan_1419@hotmail.com>
2019-05-24 01:53:13 -0300

    Removendo campos que não serão usados na api para registrar novo chamado e preparando serialize com Empreendimento > Bloco > Apartamento

--- sistema/serializers.py
+++ sistema/serializers.py
@@ -31,6 +31,36 @@ class ApartamentoSerializer(serializers.ModelSerializer):
         read_only_fields = ('id',)
 
 
+
+
+class NestedEmpreendimentoSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Empreendimento
+        fields = ['nomeEmpreendimento', 'id']
+
+
+
+class NestedBlocoSerialize(serializers.ModelSerializer):
+    empreendimento = NestedEmpreendimentoSerialize(many=False, read_only=True)
+    class Meta:
+        model = Bloco
+        fields = ['id', 'bloco', 'empreendimento']
+
+
+
+class ApartamentoProprietarioSerializer(serializers.ModelSerializer): #Serializa os apartamentos pelo id do proprietario
+    """Serializer to map the Model instance into JSON format."""
+    bloco = NestedBlocoSerialize(many=False, read_only=True)
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Apartamento
+        fields = ['id', 'apartamento', 'proprietario', 'bloco']
+
+
+
+
+
+'''
 class ApartamentoProprietarioSerializer(serializers.ModelSerializer): #Serializa os apartamentos pelo id do proprietario
     """Serializer to map the Model instance into JSON format."""
     bloco = serializers.StringRelatedField(many=False)
@@ -38,7 +68,7 @@ class ApartamentoProprietarioSerializer(serializers.ModelSerializer): #Serializa
         """Meta class to map serializer's fields with the model fields."""
         model = Apartamento
         fields = '__all__'
-
+'''
 
 class CategoriaDeProblemaSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
@@ -53,11 +83,10 @@ class CategoriaDeProblemaSerializer(serializers.ModelSerializer):
 
 class ChamadoSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
-
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Chamado
-        fields = '__all__'
+        fields = ['categoriaProblema', 'usuario', 'envolveAreaComum', 'areaComum', 'apartamento', 'descricao', 'img']
         read_only_fields = ('id',)
 
 
